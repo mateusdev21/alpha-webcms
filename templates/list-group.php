d5c263b45736ba72ccfef1fc42811977ea7f6656
frontend-2 <mateus.dev21@gmail.com>
2021-11-10 16:53:38 +0700

    add group list

--- templates/list-group.php
+++ templates/list-group.php
@@ -1,6 +1,6 @@
 <div class="list-group">
-    <a href="dashboard.php" id="dashboard-list" class="list-group-item text-capitalize">dashboard</a>
-    <a href="pages.php" id="pages-list" class="list-group-item text-capitalize">pages</a>
-    <a href="posts.php" id="posts-list" class="list-group-item text-capitalize">posts</a>
-    <a href="users.php" id="users-list" class="list-group-item text-capitalize">users</a>
+    <a href="dashboard.php" id="dashboard-list" class="list-group-item text-capitalize"><i class="bi bi-speedometer"></i>&nbsp;dashboard</a>
+    <a href="pages.php" id="pages-list" class="list-group-item text-capitalize"><i class="bi bi-file-earmark-text"></i>&nbsp;pages<span class="badge badge-light">4</span></a>
+    <a href="posts.php" id="posts-list" class="list-group-item text-capitalize"><i class="bi bi-pencil-square"></i>&nbsp;posts</a>
+    <a href="users.php" id="users-list" class="list-group-item text-capitalize"><i class="bi bi-people"></i>&nbsp;users</a>
 </div>
